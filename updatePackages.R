5ed11bdce4433e4b35942276567eb1eb5e514cce
John Lewis <lewisjr2@gmail.com>
2018-09-10 20:40:32 -0600

    modified script to handle R version 3.5.1

--- updatePackages.R
+++ updatePackages.R
@@ -4,23 +4,22 @@
 system('brew update && brew upgrade')
 
 #Find all the current and past library paths
-<<<<<<< Updated upstream
-vrs_pth<-file.path('','Library', 'Frameworks', 'R.framework', 'Versions') #these are default mac paths R, you may need to change
-=======
-vrs_pth<-file.path(.libPaths(), "..","..","..") 
->>>>>>> Stashed changes
-versions<-dir(vrs_pth)[!dir(vrs_pth)=='Current']
-o<-order(as.numeric(versions))
-cur<-versions[max(o)]
-old<-versions[-max(o)]
-libs_new<-file.path(vrs_pth,cur, 'Resources', 'library')
+
+vrs_pth <- file.path('','Library', 'Frameworks', 'R.framework', 'Versions') #these are default mac paths R, you may need to change
+
+versions <- dir(vrs_pth)[!dir(vrs_pth)=='Current']
+o <- order(as.numeric(versions))
+#cur <- versions[max(o)]
+old <- versions #[-max(o)]
+libs_new <- .libPaths()[1]
+
 #for each old library path, compare each of the libraries to the libraries in current version. Copy old to new if doesn't already exist.
 sapply(old, function(oldnumber){
-  libs_old<-file.path(vrs_pth,oldnumber, 'Resources', 'library')
-  diff_libs<-setdiff(dir(libs_old),dir(libs_new))
+  libs_old <- file.path(vrs_pth,oldnumber, 'Resources', 'library')
+  diff_libs <- setdiff(dir(libs_old),dir(libs_new))
   if(length(diff_libs)>0){
     sapply(diff_libs, function(x){
-      file.rename(from=file.path(libs_old,x),to=file.path(libs_new,x))
+      file.rename(from = file.path(libs_old,x), to = file.path(libs_new,x))
     })
   }
 })
